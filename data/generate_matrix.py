bf3fe9500b63417f6c0c3373e60b52e4ddee7fa5
JoeKifle <joey.kifle@gmail.com>
2021-12-31 15:35:03 +0100

    add M6 matrix

--- data/generate_matrix.py
+++ data/generate_matrix.py
@@ -49,9 +49,6 @@ np.savetxt("x0_m2.txt", x0_m2)
 print("[success] x0_m2 generated and saved.")
  
 
-
-
-
 # Generate M3 matrix
 M3 = np.random.randn(100, 1000) 
 np.savetxt("M3.txt", M3)
@@ -81,3 +78,22 @@ np.savetxt("M5.txt", M5)
 x0_m5 = np.round(np.random.randn(100),decimals = 3)
 np.savetxt("x0_m5.txt", x0_m5)
 print("[success] x0_m5 generated and saved.") 
+
+# Generate M6 matrix
+cond_P = 1e18     # Condition number
+log_cond_P = np.log(cond_P)
+n= 1000
+exp_vec = np.arange(-log_cond_P/4., log_cond_P * (n )/(4 * (n - 1)), log_cond_P/(2.*(n-1)))
+s = np.exp(exp_vec)
+S = np.diag(s)
+U, _ = la.qr((np.random.rand(n, n) - 5.) * 200)
+V, _ = la.qr((np.random.rand(n, n) - 5.) * 200)
+P = U.dot(S).dot(V.T)
+P = P.dot(P.T) / 1e7
+
+np.savetxt("M6.txt", P)
+print("[success] M6 generated and saved.")
+# generate x0_m1 matrix
+x0_m1 = np.round(np.random.randn(n),decimals = 3)
+np.savetxt("x0_m6.txt", x0_m1)
+print("[success] x0_m6 generated and saved.")
